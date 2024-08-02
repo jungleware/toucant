ea70a0465ffca7050f69eb7a110f0f73ea503c65
Lars vom Bruch <lrsvombruch@gmail.com>
2024-08-02 14:07:00 +0200

    added splash and launcher icons

---
+++ mobile/lib/constants/locales.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Map<String, Locale> locales = {
+  // default
+  'English (en_US)': Locale('en', 'US'),
+  // supported
+  'German (de_DE)': Locale('de', 'DE'),
+};
---
+++ mobile/lib/constants/toucant_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color toucantPrimaryColor = Color(0xFF2A453B);
+const Color toucantDarkPrimaryColor = Color(0xFF2A453B);
+const Color toucantBackgroundColor = Color(0xFF2D2D2D);
+const Color toucantDarkBackgroundColor = Color(0xFF2A453B);
